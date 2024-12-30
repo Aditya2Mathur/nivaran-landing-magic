8859f2cc24c2b516fd01bfb0b346b073fcdf23ed
Aman pandit <84219211+aman5874@users.noreply.github.com>
2024-12-30 23:07:51 +0530

    added sitemap and build fixes

--- vite.config.ts
+++ vite.config.ts
@@ -9,10 +9,12 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: 'build', // Ensure the output directory is set to 'build'
+  },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
